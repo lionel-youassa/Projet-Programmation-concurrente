d006f340fb00fbc6e91f165277fae12a0de30970
JoeviSf <joevinio.missamou@2027.ucac-icam.com>
2024-12-03 23:48:58 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/RestaurantManagement.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/siefo/Desktop/X2/Projet-Programmation-concurrente/Controller/ControleurService.cpp" "CMakeFiles/RestaurantManagement.dir/Controller/ControleurService.cpp.obj" "gcc" "CMakeFiles/RestaurantManagement.dir/Controller/ControleurService.cpp.obj.d"
+  "C:/Users/siefo/Desktop/X2/Projet-Programmation-concurrente/Model/PostgreSQLConnection.cpp" "CMakeFiles/RestaurantManagement.dir/Model/PostgreSQLConnection.cpp.obj" "gcc" "CMakeFiles/RestaurantManagement.dir/Model/PostgreSQLConnection.cpp.obj.d"
+  "C:/Users/siefo/Desktop/X2/Projet-Programmation-concurrente/cmake-build-debug/RestaurantManagement_autogen/mocs_compilation.cpp" "CMakeFiles/RestaurantManagement.dir/RestaurantManagement_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/RestaurantManagement.dir/RestaurantManagement_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/siefo/Desktop/X2/Projet-Programmation-concurrente/View/InterfaceAcceuil.cpp" "CMakeFiles/RestaurantManagement.dir/View/InterfaceAcceuil.cpp.obj" "gcc" "CMakeFiles/RestaurantManagement.dir/View/InterfaceAcceuil.cpp.obj.d"
+  "C:/Users/siefo/Desktop/X2/Projet-Programmation-concurrente/main.cpp" "CMakeFiles/RestaurantManagement.dir/main.cpp.obj" "gcc" "CMakeFiles/RestaurantManagement.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RestaurantManagement_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RestaurantManagement_autogen"
+  "RestaurantManagement_autogen/mocs_compilation.cpp"
+  "RestaurantManagement_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/RestaurantManagement_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
